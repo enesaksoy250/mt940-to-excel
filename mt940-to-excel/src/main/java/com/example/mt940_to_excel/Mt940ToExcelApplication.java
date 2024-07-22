fb7281c6110ae79d062b59b1507559025771f195
Enes Aksoy <enesaksoy250@gmail.com>
2024-07-22 13:16:22 +0300

    Dosyalar eklendi

---
+++ mt940-to-excel/src/main/java/com/example/mt940_to_excel/Mt940ToExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mt940_to_excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mt940ToExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mt940ToExcelApplication.class, args);
+	}
+
+}
---
+++ mt940-to-excel/src/test/java/com/example/mt940_to_excel/Mt940ToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mt940_to_excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mt940ToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
