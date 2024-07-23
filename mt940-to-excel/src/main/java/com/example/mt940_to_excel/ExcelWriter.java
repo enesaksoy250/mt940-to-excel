6b8cd66b5b12284e05c6478a3d43c759e9656151
Enes Aksoy <enesaksoy250@gmail.com>
2024-07-23 18:07:21 +0300

    Update ExcelWriter.java

--- mt940-to-excel/src/main/java/com/example/mt940_to_excel/ExcelWriter.java
+++ mt940-to-excel/src/main/java/com/example/mt940_to_excel/ExcelWriter.java
@@ -1,6 +1,7 @@
 package com.example.mt940_to_excel;
 
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -14,37 +15,48 @@ public class ExcelWriter {
 
     public void writeToExcel(List<MT940Transaction> transactions,String filePath) throws IOException {
 
-          Workbook workbook = new XSSFWorkbook();
-          Sheet sheet = workbook.createSheet("Transactions");
+        Workbook workbook;
+        Sheet sheet;
 
-          Row header = sheet.createRow(0);
-          header.createCell(0).setCellValue("Banka Kodu");
-          header.createCell(1).setCellValue("Şube Kodu");
-          header.createCell(2).setCellValue("Hesap Kodu");
-          header.createCell(3).setCellValue("İşlem Tutarı");
-          header.createCell(4).setCellValue("İşlem Açıklaması");
 
-          int rowNum = 1;
+        try (FileInputStream fileIn = new FileInputStream(filePath)) {
+            workbook = new XSSFWorkbook(fileIn);
+            sheet = workbook.getSheetAt(0);
+        } catch (IOException e) {
+            workbook = new XSSFWorkbook();
+            sheet = workbook.createSheet("Transactions");
+            createHeader(sheet);
+        }
 
-          for (MT940Transaction transaction : transactions) {
 
-              Row row = sheet.createRow(rowNum++);
-              row.createCell(0).setCellValue(transaction.getBankaKodu());
-              row.createCell(1).setCellValue(transaction.getSubeKodu());
-              row.createCell(2).setCellValue(transaction.getHesapKodu());
-              row.createCell(3).setCellValue(transaction.getIslemTutari());
-              row.createCell(4).setCellValue(transaction.getIslemAciklamasi());
+        int rowNum = sheet.getLastRowNum() + 1; // Satır numarası sıfırdan başladığı için +1
 
-          }
 
-       try(FileOutputStream fileOut = new FileOutputStream(filePath)) {
+        for (MT940Transaction transaction : transactions) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(transaction.getBankaKodu());
+            row.createCell(1).setCellValue(transaction.getSubeKodu());
+            row.createCell(2).setCellValue(transaction.getHesapKodu());
+            row.createCell(3).setCellValue(transaction.getIslemTutari());
+            row.createCell(4).setCellValue(transaction.getIslemAciklamasi());
+        }
 
-           workbook.write(fileOut);
 
-       }
-
-       workbook.close();
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        } finally {
+            workbook.close();
+        }
     }
 
+
+    private void createHeader(Sheet sheet) {
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("Banka Kodu");
+        header.createCell(1).setCellValue("Şube Kodu");
+        header.createCell(2).setCellValue("Hesap Kodu");
+        header.createCell(3).setCellValue("İşlem Tutarı");
+        header.createCell(4).setCellValue("İşlem Açıklaması");
+    }
 
 }
